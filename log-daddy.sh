f8856371138f33822be434c9b44ddf01aad03bb5
Joe Bowers <joe.bowers@coreos.com>
2016-04-18 09:25:52 -0700

    even stupider, script now lingers annoyingly on exit

--- log-daddy.sh
+++ log-daddy.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 NUMBER=0
+SLOW_EXIT_SECONDS=${SLOW_EXIT_SECONDS:-2}
+
+slow_exit() {
+    trap - EXIT INT TERM
+    echo "Slowly exiting (${SLOW_EXIT_SECONDS} seconds)"
+    sleep "${SLOW_EXIT_SECONDS}"
+    echo "bye!"
+    exit
+}
+trap slow_exit EXIT INT TERM
 
 while true
 do
